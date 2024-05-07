228390c4bf6c8035e3445148cff3b53c8dbcfaba
bb84729 <bb84729@gmail.com>
2024-05-07 12:56:08 +0800

    modified urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path('accounts/',include("accounts.urls")),
-    # path('accounts/', include('allauth.urls')), 
   ]
+
+    # path('accounts/', include('allauth.urls')), 
