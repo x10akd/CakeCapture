fe18cb5a73bcc2c2bca925885201646705555f0f
Wheat Wu <hiw40143113@gmail.com>
2024-05-10 14:01:42 +0800

    新增商品列表頁/第三方登入資料庫

--- core/settings.py
+++ core/settings.py
@@ -75,6 +75,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "OPTIONS": {
+            # "service": "my_service",
             "passfile": ".my_pgpass",
         },
         "NAME": env("DB_NAME"),
