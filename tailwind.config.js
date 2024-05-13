fd45f48c8f852932903b93487a87468602c2733d
NoraSeirei <bell.novtoh@gmail.com>
2024-05-13 14:15:07 +0800

    Modify 1.tailwind.config.js 2.cart_list.html 3.cart_payment.html 4.cart_confirm.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.html", "./**/*.py"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+    content: ["./templates/**/*.html", "./**/*.py"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+    safelist: [
+        // 'bg-sky-300',
+        // 'bg-violet-500',
+        // 'justify-start'
+    ],
+}
+
