fa88a1178ba8888997d12e09e4f16a51392e3ef1
bb84729 <bb84729@gmail.com>
2024-05-13 12:19:28 +0800

    remove toast

--- src/scripts/input.js
+++ src/scripts/input.js
@@ -2,7 +2,7 @@ import Swal from "sweetalert2";
 import Alpine from "alpinejs";
 import "./alert.js";
 
-window.Toast = Toast;
+// window.Toast = Toast;
 window.Alpine = Alpine;
 window.Swal = Swal;
 
