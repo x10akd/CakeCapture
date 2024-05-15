fff0f6740ab8514cb850c64fa5e1ab1b84775007
Chien Chuan Wang <chien@chienchuan.com>
2024-05-15 20:21:07 +0800

    WIP: Added chatroom.html and related functior for rendering

--- pages/urls.py
+++ pages/urls.py
@@ -8,6 +8,12 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("my_favorite/", views.my_favorite, name="my_favorite"),
     path("cart/detail", views.cart_detail, name="cart_detail"),
+    path("item_detail", views.detail, name="detail"),
+    path("cart/list", views.cart_list, name="cart_list"),
+    path("cart/confirm", views.cart_confirm, name="cart_confirm"),
+    path("cart/payment", views.cart_payment, name="cart_payment"),
+    path("cart/checkout", views.cart_checkout, name="cart_checkout"),
+    path("chat", include("chat.urls")),
 ]
 
 
