66e854b0be512f5f4720a21ff5c0899c208ea3fc
Chien Chuan Wang <chien@chienchuan.com>
2024-05-17 22:49:46 +0800

    Edited views.py for rendering the chatroom statically

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import *
 
 
+# @login_required
 def chat_view(request):
-    return render(request, "chat/chatroom.html")
+    chat_group = get_object_or_404(ChatGroup, group_name="public-chat")
+    chat_messages = chat_group.chat_messages.all()[:30]
+    form = ChatmessageCreateForm()
+
+    if request.method == "POST":
+        form = ChatmessageCreateForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.author = request.user
+            message.group = chat_group
+            message.save()
+            return redirect("home")
+
+    return render(
+        request, "chat/chatroom.html", {"chat_messages": chat_messages, "form": form}
+    )
