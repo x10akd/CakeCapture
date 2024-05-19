dec77b9a33c7e67ccb43ccfe6af9d98bf6ffa153
Chien Chuan Wang <chien@chienchuan.com>
2024-05-19 11:26:35 +0800

    Fixed styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.html", "./**/*.py"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-  safelist: [
-    "w-[180px]",
-    "h-[180px]",
-  ]
-};
+    content: ["./templates/**/*.html", "./**/*.py"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+    safelist: [
+    ],
+}
+
