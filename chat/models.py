a35a2d83bbcd9e661496934278f1556c1d9c7cc2
Chien Chuan Wang <chien@chienchuan.com>
2024-05-20 11:41:02 +0800

    Modified created of GroupMessage in models.py

--- chat/models.py
+++ chat/models.py
@@ -21,7 +21,7 @@ class GroupMessage(models.Model):
     )
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.CharField(max_length=300)
-    created = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.author.username} : {self.body}"
