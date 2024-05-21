e8c24281be7ca31901e7b09ed7f4e9bb113135d3
Chien Chuan Wang <chien@chienchuan.com>
2024-05-21 14:17:22 +0800

    Modified settings.py by removing unnecessary comments, codes, etc

--- core/settings.py
+++ core/settings.py
@@ -6,21 +6,13 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-y1gkrdf9wpvr#^l$^-6nx7sf2h+y-t58y_nhfs@3c6$a+!#&la"
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
-
-
-# Application definition
 
 INSTALLED_APPS = [
     "daphne",
@@ -44,6 +36,8 @@ INSTALLED_APPS = [
     "chats",
 ]
 
+SITE_ID = 5
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -52,6 +46,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # allauth-related
     "allauth.account.middleware.AccountMiddleware",
     "django_htmx.middleware.HtmxMiddleware",
 ]
@@ -69,6 +64,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                # carts-related
                 "cart.context_processors.cart",
             ],
         },
@@ -77,13 +73,6 @@ TEMPLATES = [
 
 # WSGI_APPLICATION = "core.wsgi.application"
 ASGI_APPLICATION = "core.asgi.application"
-
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
-    }
-}
-
 
 DATABASES = {
     "default": {
@@ -99,25 +88,20 @@ DATABASES = {
     }
 }
 
-
 AUTH_PASSWORD_VALIDATORS = [
     # {
-    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    #     "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     # },
     # {
-    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    #     "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     # },
     # {
-    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    #     "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     # },
     # {
-    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    #     "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     # },
 ]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
 
@@ -127,28 +111,19 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# settings.py
 AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
+    # allauth-related
     "allauth.account.auth_backends.AuthenticationBackend",
 )
-
-SITE_ID = 5
 
 ACCOUNT_EMAIL_VERIFICATION = "none"
 
@@ -171,11 +146,10 @@ SOCIALACCOUNT_PROVIDERS = {
 # 定義 SESSION 跟 cookie 的存活時間, 單位為秒
 SESSION_COOKIE_AGE = 60 * 60 * 24 * 30
 
-# 設定登入後, 使用者會導向首頁
 LOGIN_REDIRECT_URL = "/"
 LOGIN_URL = "login"
 
-# email 設定
+# Related to reset email sender
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
