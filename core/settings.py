deb16e8beb010ee8095a36e474bd43d09591ce59
Wheat Wu <hiw40143113@gmail.com>
2024-05-25 09:35:06 +0800

    [remove]:remove annotation

--- core/settings.py
+++ core/settings.py
@@ -75,7 +75,6 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "OPTIONS": {
-            # "service": "my_service",
             "passfile": ".my_pgpass",
         },
         "NAME": env("DB_NAME"),
