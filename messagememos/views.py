b55ef715ec4586651c93a5dbadffbef2a16d3a24
Chien Chuan Wang <chien@chienchuan.com>
2024-05-26 10:45:14 +0800

    [modified] Removed unnecessary codes and comments

--- messagememos/views.py
+++ messagememos/views.py
@@ -1,34 +1,16 @@
-from django.forms import BaseModelForm
-from django.http import HttpResponse
-from django.shortcuts import render,redirect
-from django.contrib.auth.models import User
-from django.core.exceptions import ObjectDoesNotExist
-from django.contrib import messages
+from django.shortcuts import render, redirect
 from .models import MessageModel
 
-# Create your views here.
 
 def create(request):
-      if request.method == 'POST':
-        message = request.POST.get('mytext')
+    if request.method == "POST":
+        message = request.POST.get("mytext")
         if message:
-            new_message = MessageModel(
-                message=message,
-                user=request.user
-            )
+            new_message = MessageModel(message=message, user=request.user)
             new_message.save()
-      return redirect('accounts:user')
+    return redirect("accounts:user")
 
 
 def message(request):
-    comments = MessageModel.objects.all()  # 獲取所有留言
-    return render(request, 'user.html', {'comments': comments})
-    
-
-
-
-
-
-
-
-
+    comments = MessageModel.objects.all()
+    return render(request, "user.html", {"comments": comments})
