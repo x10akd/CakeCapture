aa5a204742d4864e9d8636a66c75be87428cac6d
blueblue <bb84729@gmail.com>
2024-05-30 14:55:07 +0800

    [modify]add space

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product, ProductReview,Favorite
+from .models import Category, Product, ProductReview, Favorite
 
 admin.site.register(Category)
 admin.site.register(Product)
