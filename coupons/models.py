2b9d278f076bdd3fa52016aa10b2b1a884fb3f02
Chien Chuan Wang <chien@chienchuan.com>
2024-06-03 20:44:37 +0800

    [modified] Allowed expired_at and order columns to be blank

--- coupons/models.py
+++ coupons/models.py
@@ -10,7 +10,7 @@ class Coupon(models.Model):
     min_order = models.IntegerField(blank=True, null=True)
     usage_limit = models.IntegerField(blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    expired_at = models.DateTimeField(auto_now=True)
+    expired_at = models.DateTimeField(blank=True)
 
     def __str__(self):
         return self.code
@@ -19,7 +19,7 @@ class Coupon(models.Model):
 class UserCoupon(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     coupon = models.ForeignKey(Coupon, on_delete=models.CASCADE)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True, blank=True)
     used_at = models.DateTimeField(auto_now=True)
     usage_count = models.IntegerField(default=0, blank=False)
 
