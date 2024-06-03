2a9176a226e0356b776c6e55ec48bc41b0f4320c
Chien Chuan Wang <chien@chienchuan.com>
2024-06-03 20:43:36 +0800

    [modified] Configurated files in order to make signals.py work

--- coupons/apps.py
+++ coupons/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class CouponsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'coupons'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "coupons"
+
+    def ready(self):
+        import coupons.signals
