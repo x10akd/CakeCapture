89fe5430b4745595ad640fb34d84312c98de32da
Chien Chuan Wang <chien@chienchuan.com>
2024-06-03 20:42:33 +0800

    [added] Created signals.py for distributing a free coupon after registration

---
+++ coupons/signals.py
@@ -0,0 +1,25 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import *
+from django.utils import timezone
+from datetime import timedelta
+
+
+@receiver(post_save, sender=User)
+def create_coupon_for_new_user(sender, instance, created, **kwargs):
+    if created:
+        coupon = Coupon.objects.create(
+            code="5XCAMP",
+            discount=50,
+            min_order=0,
+            usage_limit=1,
+            expired_at=timezone.now() + timedelta(days=30),
+        )
+
+    UserCoupon.objects.create(
+        user=instance,
+        coupon=coupon,
+        order=None,
+        usage_count=0,
+    )
