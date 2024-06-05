d1f7db430c8e057dbe30386238429de78ee04af6
Chien Chuan Wang <chien@chienchuan.com>
2024-06-05 20:30:04 +0800

    [modified] Adjusted coupons/models.py

--- coupons/models.py
+++ coupons/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from orders.models import *
 
 
@@ -9,8 +10,6 @@ class Coupon(models.Model):
     discount = models.IntegerField(blank=False)
     min_order = models.IntegerField(blank=True, null=True)
     usage_limit = models.IntegerField(blank=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    expired_at = models.DateTimeField(blank=True)
 
     def __str__(self):
         return self.code
@@ -21,7 +20,17 @@ class UserCoupon(models.Model):
     coupon = models.ForeignKey(Coupon, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True, blank=True)
     used_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expired_at = models.DateTimeField(blank=True)
     usage_count = models.IntegerField(default=0, blank=False)
+
+    def clean(self):
+        if self.usage_count > self.coupon.usage_limit:
+            raise ValidationError("此優惠券使用次數已達上限")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.user.username} - {self.coupon.code}"
