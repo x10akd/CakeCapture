b8293f6e305d741b0ddd74fb217fea4bdc2fccee
Wheat Wu <hiw40143113@gmail.com>
2024-06-10 18:09:09 +0800

    [fixed] fix rwd problem

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.html", "./**/*.py"],
-  theme: {
-    extend: {},
-    screens: {
-      'lg': '1024px',
-      'md': '768px',
-      'sm': '640px'
+    content: ["./templates/**/*.html", "./**/*.py"],
+    theme: {
+        extend: {},
+        screens: {
+            'lg': '1024px',
+            'md': '768px',
+            'sm': '640px'
+        },
     },
-  },
-  plugins: [],
-  safelist: [],
+    plugins: [],
+    safelist: [],
 };
