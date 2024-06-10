f4e36ae0100a6740a23b55bee100791eaf6dbe19
blueblue <bb84729@gmail.com>
2024-06-10 18:33:18 +0800

    Update custom_fields.py

--- products/custom_fields.py
+++ products/custom_fields.py
@@ -15,13 +15,13 @@ class CompressedImageField(models.ImageField):
     def pre_save(self, model_instance, add):
         file = super().pre_save(model_instance, add)
         if file and hasattr(file, 'file'):
-            # 打开图像并压缩
+            
             image = Image.open(file)
             buffer = io.BytesIO()
             image.save(buffer, 'JPEG', quality=self.quality)
             buffer.seek(0)
-            # 上传到S3
+           
             s3.upload_fileobj(buffer, 'cakecapture', file.name, ExtraArgs={'ContentType': 'image/jpeg'})
-            # 设置文件字段
+            
             model_instance.__dict__[self.name] = file.name
         return file
