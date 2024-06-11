43e24bb7f7ad44d06d4d7ec6e9e0223bd5ebe909
Chien Chuan Wang <chien@chienchuan.com>
2024-06-11 15:32:10 +0800

    [modfied] Added RWD config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./templates/**/*.html", "./**/*.py"],
-    theme: {
-        extend: {},
+  content: ["./templates/**/*.html", "./**/*.py"],
+  theme: {
+    extend: {},
+    screens: {
+      'lg': '1024px',
+      'md': '768px',
+      'sm': '640px'
     },
-    plugins: [],
-    safelist: [],
+  },
+  plugins: [],
+  safelist: [],
 };
