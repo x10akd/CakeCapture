3575cc96600b8cc5d74f1538a061e5174117f36c
Chien Chuan Wang <chien@chienchuan.com>
2024-06-13 20:51:49 +0800

    [modified] Added HTMX

--- src/scripts/input.js
+++ src/scripts/input.js
@@ -4,6 +4,7 @@ import Alpine from "alpinejs";
 import { togglePaymentNotice1, togglePaymentNotice2 } from "./order_notice.js";
 import { Toast } from "./alert.js"
 import { carousel } from "./carousel.js";
+import 'htmx.org'
 
 window.Toast = Toast;
 window.Alpine = Alpine;
@@ -11,5 +12,6 @@ window.Swal = Swal;
 window.togglePaymentNotice1 = togglePaymentNotice1
 window.togglePaymentNotice2 = togglePaymentNotice2
 window.carousel = carousel
+window.htmx = require('htmx.org');
 
 Alpine.start();
