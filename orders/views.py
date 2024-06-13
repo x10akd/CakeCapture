8fe581768e45699c29c2c94410c2ef6bb2037877
Chien Chuan Wang <chien@chienchuan.com>
2024-06-13 13:32:38 +0800

    [modified] Finished LINE Pay request

--- orders/views.py
+++ orders/views.py
@@ -14,6 +14,12 @@ from orders import ecpay_payment_sdk
 import environ
 import importlib.util
 from accounts.models import *
+import secrets
+import json
+import hmac
+import hashlib
+import base64
+import requests
 
 spec = importlib.util.spec_from_file_location(
     "ecpay_payment_sdk", "orders/ecpay_payment_sdk.py"
@@ -328,23 +334,27 @@ def line_pay_request(request):
     if request.method == "POST":
         order_id = order.order_id
         package_id = order.pk
+        url = f"{env('LINE_SANDBOX_URL')}{env('LINE_REQUEST_URL')}"
+
+        products = [
+            {
+                "id": "1",
+                "name": "甜點",
+                "quantity": 1,
+                "price": order.total,
+            }
+        ]
+        amount = sum([product["quantity"] * product["price"] for product in products])
 
         payload = {
-            "amount": 1,
+            "amount": amount,
             "currency": "TWD",
             "orderId": order_id,
             "packages": [
                 {
                     "id": package_id,
-                    "amount": 1,
-                    "products": [
-                        {
-                            "id": "1",
-                            "name": "甜點",
-                            "quantity": 1,
-                            "price": order.total,
-                        }
-                    ],
+                    "amount": amount,
+                    "products": products,
                 }
             ],
             "redirectUrls": {
@@ -353,8 +363,47 @@ def line_pay_request(request):
             },
         }
 
+        LINE_PAY_URI = env("LINE_SIGNATURE_REQUEST_URI")
+        headers = create_line_pay_headers(payload, LINE_PAY_URI)
+        body = json.dumps(payload)
+        response = requests.post(url, headers=headers, data=body)
+
+        if response.status_code == 200:
+            data = response.json()
+            if data["returnCode"] == "0000":
+                return redirect(data["info"]["paymentUrl"]["web"])
+            else:
+                print(data["returnMessage"])
+                return render(request, "orders/line_pay_checkout.html")
+        else:
+            print(f"Error: {response.status_code}")
+            return render(request, "orders/line_pay_checkout.html")
+
     else:
         return render(request, "orders/line_pay_checkout.html")
+
+
+def create_line_pay_headers(body, uri):
+
+    nonce = secrets.token_hex(16)
+    LINE_PAY_CHANNEL_SECRET = env("LINE_PAY_CHANNEL_SECRET")
+    body_to_json = json.dumps(body)
+    message = LINE_PAY_CHANNEL_SECRET + uri + body_to_json + nonce
+
+    binary_message = message.encode()
+    binary_LINE_PAY_CHANNEL_SECRET = LINE_PAY_CHANNEL_SECRET.encode()
+
+    hash = hmac.new(binary_LINE_PAY_CHANNEL_SECRET, binary_message, hashlib.sha256)
+    signature = base64.b64encode(hash.digest()).decode()
+
+    headers = {
+        "Content-Type": "application/json",
+        "X-LINE-ChannelId": env("LINE_PAY_CHANNEL_ID"),
+        "X-LINE-Authorization-Nonce": nonce,
+        "X-LINE-Authorization": signature,
+    }
+
+    return headers
 
 
 def line_pay_confirm(request):
